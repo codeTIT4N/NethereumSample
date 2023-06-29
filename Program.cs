bd5b4acf064eba29830a9a547fc4bca9a7f9de05
codeTIT4N <lokeshkumar636@yahoo.com>
2023-06-29 17:25:45 +0530

    Readme + small fixes

--- Program.cs
+++ Program.cs
@@ -9,24 +9,25 @@ namespace NethereumSample
             DotNetEnv.Env.Load();
             var infura = Environment.GetEnvironmentVariable("INFURA");
             var pvtKey = Environment.GetEnvironmentVariable("PVT_KEY");
+            var toAddress = Environment.GetEnvironmentVariable("TO_ADDRESS");
             GetAccountBalance(infura).Wait();
-            SendTransaction(infura, pvtKey).Wait();
+            SendTransaction(infura, pvtKey, toAddress).Wait();
         }
 
         static async Task GetAccountBalance(string? infura)
         {
-            var web3 = new Web3($"https://goerli.infura.io/v3/{infura}");
+            var web3 = new Web3(infura);
             var balance = await web3.Eth.GetBalance.SendRequestAsync(
                 "0xFc768Ce3d4c838eF32e7D9b2116431F7a1Be3A6D"
             );
-            Console.WriteLine($"Balance in Wei: {Web3.Convert.FromWei(balance.Value)} ETH");
+            Console.WriteLine($"Balance: {Web3.Convert.FromWei(balance.Value)} ETH");
         }
 
-        static async Task SendTransaction(string? infura, string? pvtKey)
+        static async Task SendTransaction(string? infura, string? pvtKey, string? toAddress)
         {
-            var account = new Nethereum.Web3.Accounts.Account($"0x{pvtKey}");
-            var web3 = new Web3(account, $"https://goerli.infura.io/v3/{infura}");
-            var toAddress = "0xc9D506209f57948a0C0df6ED45621Fb47572Af99";
+            Console.WriteLine($"Sending ether to {toAddress}...");
+            var account = new Nethereum.Web3.Accounts.Account($"{pvtKey}");
+            var web3 = new Web3(account, infura);
             var transaction = await web3.Eth
                 .GetEtherTransferService()
                 .TransferEtherAndWaitForReceiptAsync(toAddress, 0.1m);
---
+++ README.md
@@ -0,0 +1,11 @@
+# NethereumSample
+
+Interacting with the ethereum blockchain in C# using nethereum.
+
+### To use
+
+- Step 1: Clone the repo: `git clone https://github.com/codeTIT4N/NethereumSample`
+- Step 2: Build project: `dotnet build`
+- Step 3: Create `.env` using `.env.example`: `cp .env.example .env`
+- Step 4: Update `.env`
+- Step 5: Run the project: `dotnet run`
